f02907f4a5325f07163f4a6f471bffc24540094d
marianaceotto <marianaceott@gmail.com>
2023-02-28 19:51:47 -0300

    Entrega-Ex

---
+++ tests/pratica3.test.ts
@@ -0,0 +1,12 @@
+import {pratica3} from "../src/pratica3"
+
+describe("Testar o arquivo pratica3.ts", () => {
+    test("Função que recebe dois números e retorna um objeto com propriedade", () => {
+        const result = pratica3(3, 5)
+        const expectResult = {
+            soma: 8,
+            multi: 15
+        }
+        expect(result).toEqual(expectResult)
+    })
+})
